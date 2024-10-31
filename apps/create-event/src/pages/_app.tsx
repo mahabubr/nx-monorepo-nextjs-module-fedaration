e70748e459b9e50cacec0373e25d4dc018df6daf
mahabubr <mahabub.r753@gmail.com>
2024-10-31 15:04:51 +0600

    Store Connection Removed

--- apps/create-event/src/pages/_app.tsx
+++ apps/create-event/src/pages/_app.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable @nx/enforce-module-boundaries */
 'use client';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-import { Provider } from 'react-redux';
-import { store } from '../../../../shared-state/src';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,9 +11,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to create-event!</title>
       </Head>
       <main className="app">
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
       </main>
     </>
   );
--- apps/view-event/src/pages/_app.tsx
+++ apps/view-event/src/pages/_app.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable @nx/enforce-module-boundaries */
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-import { store } from '../../../../shared-state/src';
-import { Provider } from 'react-redux';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,9 +9,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to view-event!</title>
       </Head>
       <main className="app">
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
       </main>
     </>
   );
