0cb1e4a7ee0801d47c195a53d4e11d417dfaa517
mahabubr <mahabub.r753@gmail.com>
2024-10-31 16:00:11 +0600

    Remove Module Fedaration from other

--- apps/create-event/next.config.js
+++ apps/create-event/next.config.js
@@ -1,14 +1,35 @@
-process.env.NEXT_PRIVATE_LOCAL_WEBPACK = 'true';
+process.env.NEXT_PRIVATE_LOCAL_WEBPACK = true;
 
 const { composePlugins, withNx } = require('@nx/next');
-const applyModuleFederation = require('./webpack.config');
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
 const nextConfig = {
   nx: {
     svgr: false,
   },
-  webpack: (config, options) => {
-    return applyModuleFederation(config);
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'create',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: {
+          view: `view@http://localhost:3001/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+          single: `single@http://localhost:3002/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+        },
+        exposes: {
+          './Card': './src/components/Card.tsx',
+        },
+
+        shared: {},
+      })
+    );
+
+    return config;
   },
 };
 
--- apps/view-event/next.config.js
+++ apps/view-event/next.config.js
@@ -1,14 +1,8 @@
-process.env.NEXT_PRIVATE_LOCAL_WEBPACK = 'true';
-
 const { composePlugins, withNx } = require('@nx/next');
-const applyModuleFederation = require('./webpack.config');
 
 const nextConfig = {
   nx: {
     svgr: false,
-  },
-  webpack: (config, options) => {
-    return applyModuleFederation(config);
   },
 };
 
